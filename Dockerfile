a6a437b79c587edc4c3024775bbe5ec3001b5423
saulius.alisauskas <saulius.alisauskas@ericsson.com>
2016-12-19 22:59:21 -0500

    install keyrings.alt and pass all params to run script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3
 MAINTAINER SauliusAlisauskas <alisauskas.saulius@gmail.com>
 
-RUN pip install pycrypto; pip install jrnl[enctrypted]
+RUN pip install keyrings.alt pycrypto jrnl[enctrypted]
 
 # Create new user with same id as user that build container
 # so that container would have same access to files
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # 	JOURNAL_FILES_PATH - path to where journal files defined in .jrnl_config are saved
 
 source .config
-docker run -it --rm -u jrnl --name=jrnl -v ~/.jrnl_config:/home/jrnl/.jrnl_config -v ${JOURNAL_FILES_PATH}:${JOURNAL_FILES_PATH} swampy/jrnl jrnl $1
+docker run -it --rm -u jrnl --name=jrnl -v ~/.jrnl_config:/home/jrnl/.jrnl_config -v ${JOURNAL_FILES_PATH}:${JOURNAL_FILES_PATH} swampy/jrnl jrnl $@
